98dcb678ab78ede11b3add2adf2a23c3a64ac06d
mac <mac@192.168.144.141>
2024-03-02 11:48:05 +0530

    new commit

--- src/Test/Hello.java
+++ src/Test/Hello.java
@@ -5,11 +5,9 @@ public class Hello {
 		System.out.println("hello");
 		System.out.println(2215);
 		System.out.println("my local");
-		System.out.println("suresh & abshar");
-//<<<<<<< HEAD
+
 		System.out.println("hii");
-//=======
 		System.out.println("adding new line to master repostory");
-//>>>>>>> branch 'master' of https://github.com/badboy0344/Project.git
+		System.out.println("retrying");
 	}
 }
